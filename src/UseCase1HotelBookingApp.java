74ef7e2b3614d29215ac8090daf7683506cd0af2
Elmozoop <yashraj2005@gmail.com>
2026-03-16 09:25:48 +0530

    Added UC1 - Application Entry & Welcome Message

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,11 @@
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" Welcome to the Hotel Booking System ");
+        System.out.println(" Application Version: 1.0 ");
+        System.out.println("====================================");
+
+    }
+}
